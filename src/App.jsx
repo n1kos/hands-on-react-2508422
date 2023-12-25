00881c7bdf1488d502aefa0805f60fdcc0b22bfb
n1kos <8678069+n1kos@users.noreply.github.com>
2023-12-25 09:43:48 +0000

    class based components example

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import React, {Component} from "react"
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <h1>Hello {this.props.name}</h1>
+  }
+}
+
 function App() {
   const name = 'StarGazers'
   return (
@@ -5,6 +16,9 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
+          <Welcome name="1"/>
+          <Welcome name="2" />
+          <Welcome name="3" />
           <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
