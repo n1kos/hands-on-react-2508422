2349d9177ce460294185f5d718e4a264cc0add42
n1kos <8678069+n1kos@users.noreply.github.com>
2023-12-25 12:17:06 +0000

    define event as prop, pass to child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
-import ListCast from './components/ListCast';
+import React, { useState } from "react";
+import Welcome from "./components/Welcome";
+import ListCast from "./components/ListCast";
 
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
+  let [memberInfo, setMemberInfo] = useState(null);
   return (
     <div className="container">
-      <article>
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast />
-          <Support />
-        </hgroup>
-      </article>
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <Welcome name="StarGazers" />
+        <p>
+          Members of an <b>intergalactic alliance</b>
+          <br />
+          paving the way for peace and benevolence among all species. They are
+          known for their enthusiasm for science, for their love of fun, and
+          their dedication to education.
+        </p>
+        <ListCast onChoice={(info)=>{setMemberInfo(info)}}/>
+        {memberInfo && (
+          <article>
+            <hgroup>
+              <div>
+                <img src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
+                <h1>
+                  {memberInfo.name}
+                </h1>
+                <p>{memberInfo.bio}</p>
+              </div>
+            </hgroup>
+          </article>
+        )}
+      </hgroup>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,31 +1,37 @@
 import { useState, useEffect } from "react";
 
-export default () => {
+export default ({onChoice}) => {
   const [cast, setCast] = useState([]);
+  // const { onChoice } = props; or we can destructure it in the function signature
 
   async function fetchCast() {
-    const response = await fetch('cast.json');
+    const response = await fetch("cast.json");
     setCast(await response.json());
   }
 
   useEffect(() => {
     fetchCast();
   });
-
   return (
-    <div style={{
-      display: "grid",
-      gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
-      gap: `1rem`,
-      marginBottom: '1rem'
-    }}>
-      {
-        cast.map(member => (
-          <a key={member.id} data-tooltip={member.name}>
-            <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
-          </a>
-        ))
-      }
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+        gap: `1rem`,
+        marginBottom: "1rem",
+      }}
+    >
+      {cast.map((member) => (
+        <a
+          onClick={() => {
+            onChoice(member);
+          }}
+          key={member.id}
+          data-tooltip={member.name}
+        >
+          <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
