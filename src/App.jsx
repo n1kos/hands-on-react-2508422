7a3f403e316c24fa6fd6f9026885a0c85b70a3f5
n1kos <8678069+n1kos@users.noreply.github.com>
2023-12-25 10:48:29 +0000

    and since this is default, it's unnamed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Welcome from "./components/Welcome"
+import WelcomAnything from "./components/Welcome"
 
 function App() {
   const name = 'StarGazers'
@@ -7,9 +7,9 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="1"/>
-          <Welcome name="2" />
-          <Welcome name="3" />
+          <WelcomAnything name="1"/>
+          <WelcomAnything name="2" />
+          <WelcomAnything name="3" />
           <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
