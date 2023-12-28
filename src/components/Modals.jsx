f87c3a9034a0a771cc678fb25823ce91172a54d9
n1kos <8678069+n1kos@users.noreply.github.com>
2023-12-28 09:59:43 +0000

    adding previous next buttons
    basically remember you can add a parameter in a callback and that will receive the return of the passed function
    
    ie handleChange on App.jsx receives the id from onClick in Modals.jsx
    
    notice the return in handleClose.

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,28 +1,57 @@
-export default ({ member, handleClose }) => {
+export default ({ member, handleClose, handleChange }) => {
   return (
     <dialog id="modal-member" open>
       <article>
-        <a href="#close"
+        <a
+          href="#close"
           aria-label="Close"
           className="close"
           data-target="modal-member"
-          onClick={handleClose}
+          onClick={() => {
+            handleClose("closed");
+          }}
         ></a>
         <hgroup>
-          <div style={{
-            display: "flex",
-            gap: '1rem'
-          }}>
-            <img style={{ width: '200px' }}
+          <div
+            style={{
+              display: "flex",
+              gap: "1rem",
+            }}
+          >
+            <img
+              style={{ width: "200px" }}
               src={`images/${member.slug}.svg`}
-              alt={member.name} />
+              alt={member.name}
+            />
             <hgroup>
               <h1>{member.name}</h1>
               <p>{member.bio}</p>
+            </hgroup>
+            <hgroup>
+              <a
+                className="outline"
+                href="#"
+                role="button"
+                onClick={() => {
+                  handleChange(member.id - 1);
+                }}
+              >
+                previous
+              </a>
+              <a
+                className="outline"
+                href="#"
+                role="button"
+                onClick={() => {
+                  handleChange(member.id + 1);
+                }}
+              >
+                next
+              </a>
             </hgroup>
           </div>
         </hgroup>
       </article>
     </dialog>
-  )
-}
+  );
+};
